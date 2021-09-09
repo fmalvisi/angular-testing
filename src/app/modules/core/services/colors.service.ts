6b5c1f1d4087cb3ad17631a6b4222375092201ef
Filippo Malvisi <fmalvisi@imolainformatica.it>
2021-09-09 15:06:20 +0200

    API mockate con JSON-Server

--- src/app/modules/core/services/colors.service.ts
+++ src/app/modules/core/services/colors.service.ts
@@ -9,21 +9,22 @@ import { map } from 'rxjs/operators';
 export class ColorsService {
   baseURL: string;
   fetchedColors: Color[] = [];
+  private localURL = "http://localhost:3000/colori"
 
   constructor(private http: HttpClient) {
-    this.baseURL = "https://reqres.in/api/colori";
+    this.baseURL = this.localURL;
   }
 
   getColorList(): Promise<Color[]> {
     console.log("chiamato getColorList");
     this.fetchedColors = [];
-    return this.http.get<any>(this.baseURL).pipe(
+    return this.http.get<Color[]>(this.baseURL).pipe(
       map(res => {
         console.log("getColorList response: ", res);
-        if (!res || !res.data) {
+        if (!res) {
 
         } else {
-          for (let currentColor of res.data) {
+          for (let currentColor of res) {
             currentColor.loaded = new Date().toISOString();
             this.fetchedColors.push(currentColor);
           }
@@ -32,30 +33,6 @@ export class ColorsService {
       })
     ).toPromise();
   }
-
-  /*getColorList(): Color[] {
-    console.log("chiamato getColorList");
-    this.fetchedColors = [];
-    this.http.get<any>(this.baseURL).subscribe(
-      res => {
-        console.log("getColorList response: ", res);
-        if (!res || !res.data) {
-
-        } else {
-          for (let currentColor of res.data) {
-            currentColor.loaded = new Date().toISOString();
-            this.fetchedColors.push(currentColor);
-          }
-        }
-        return this.fetchedColors;
-      },
-      error => {
-        console.log('getColorList reject', error);
-        alert(error);
-        return null;
-      }
-    );
-  } */
 
   getColor(id: number) {
     console.log("chiamato getColor con id: ", id);
