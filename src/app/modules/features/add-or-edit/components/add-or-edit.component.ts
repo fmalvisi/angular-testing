08e7d5e5de0336886bcd92cf6992b3842b8e4cca
Filippo Malvisi <fmalvisi@imolainformatica.it>
2021-10-05 11:23:37 +0200

    reactive forms working

--- src/app/modules/features/add-or-edit/components/add-or-edit.component.ts
+++ src/app/modules/features/add-or-edit/components/add-or-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ColorsService } from 'src/app/modules/core/services/colors.service';
@@ -9,10 +9,19 @@ import { Color } from 'src/app/modules/shared/model/color';
   templateUrl: './add-or-edit.component.html',
   styleUrls: ['./add-or-edit.component.scss']
 })
-export class AddOrEditComponent implements OnInit, OnChanges {
+export class AddOrEditComponent implements OnInit {
   currentId: string = '';
   selectedColor: Color | undefined;
-  colorForm : FormGroup = this.fb.group({});
+  colorForm : FormGroup = this.fb.group({
+    id: [''],
+    name: [''],
+    year: [''],
+    color: [''],
+    pantone_value: [''],
+    loaded: [''],
+    edited_by: [''],
+    check: [false, [Validators.requiredTrue]]
+  });
 
   constructor(private route: ActivatedRoute,
     private colorService: ColorsService,
@@ -26,31 +35,35 @@ export class AddOrEditComponent implements OnInit, OnChanges {
           this.colorService.getColor(+this.currentId).then((res: Color) => {
             console.log("colore caricato", res);
             this.selectedColor = res;
+            this.createForm();
           }).catch((error) => {
             this.selectedColor = undefined;
           })
       } else {
         //new color
         this.selectedColor = this.generateNewColorStub(); //Collisions
+        this.createForm();
       }
     })
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (!changes.selectedColor.firstChange) {
-      this.createForm();
-    }
-  }
-
   createForm() {
-    this.colorForm.addControl("id", this.fb.control(this.selectedColor?.id, [Validators.required, Validators.min(1)]));
+    this.colorForm.addControl("id", this.fb.control('', [Validators.required, Validators.min(1)]));
     this.colorForm.addControl("name", this.fb.control('', [Validators.required, Validators.minLength(1)]));
     this.colorForm.addControl("year", this.fb.control('', [Validators.min(1970)]));
     this.colorForm.addControl("color", this.fb.control('', [Validators.required, Validators.pattern("^#([a-fA-F0-9]{6})$")]));
     this.colorForm.addControl("pantone_value", this.fb.control('', [Validators.required, Validators.pattern("^([0-9]{2}-[0-9]{4})$")]));
     this.colorForm.addControl("loaded", this.fb.control('', []));
     this.colorForm.addControl("edited_by", this.fb.control('', []));
-    this.colorForm.addControl("check", this.fb.control(false, [Validators.requiredTrue]));
+    this.colorForm.patchValue({
+      id: this.selectedColor?.id,
+      name: this.selectedColor?.name,
+      year: this.selectedColor?.year,
+      color: this.selectedColor?.color,
+      pantone_value: this.selectedColor?.pantone_value,
+      loaded: this.selectedColor?.loaded,
+      edited_by: this.selectedColor?.edited_by
+    });
   }
 
   private generateNewColorStub(): Color {
